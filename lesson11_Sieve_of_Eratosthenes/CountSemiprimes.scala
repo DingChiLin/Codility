f1b5dcabe9b9e6f91e81a9b5bc2b2c52fea7405e
Arthur Lin <gn01168178@yahoo.com.tw>
2017-04-27 10:47:17 +0800

    finish count semiprimes with 100%

--- lesson11_Sieve_of_Eratosthenes/CountSemiprimes.scala
+++ lesson11_Sieve_of_Eratosthenes/CountSemiprimes.scala
@@ -1,41 +1,33 @@
 object CountSemiprimes extends App{
   println("HELLO")
 
-  //val p = Array(1,4,16)
-  //val q = Array(26,10,20)
-  //val n = 26
-  val p = Array(1)
-  val q = Array(1)
-  val n = 1
+  val p = Array(1,4,16)
+  val q = Array(26,10,20)
+  val n = 26
+  //val p = Array(1)
+  //val q = Array(1)
+  //val n = 1
   val s = solution(n, p, q)
   println(s.toList)
 
   def solution(n:Int, p: Array[Int], q:Array[Int]): Array[Int] = {
-    val semiPrimes = semiPrimesList(n)
-    val semiPrimesAccuList = (0 to n).toArray
-    var index = 0
-    for(i <- (0 to n)){
-      if(index < semiPrimes.length && i == semiPrimes(index)){
-        index += 1
-      }
-      semiPrimesAccuList(i) = index
-    }
+    val semiPrimesPreSum = semiPrimesList(n)
     val l = p.length
     val r = (0 until l).toArray
     for(i <- (0 until l)){
       val p_val = p(i)
       val q_val = q(i)
-      r(i) = countSemiPrimes(p_val, q_val, semiPrimesAccuList.toList)
+      r(i) = countSemiPrimes(p_val, q_val, semiPrimesPreSum)
     }
 
     return r
   }
 
-  def countSemiPrimes(p_val: Int, q_val:Int, semiPrimesAccuList:List[Int]): Int = {
-    return semiPrimesAccuList(q_val) - semiPrimesAccuList(p_val-1)
+  def countSemiPrimes(p_val: Int, q_val:Int, semiPrimesPreSum:Array[Int]): Int = {
+    return semiPrimesPreSum(q_val) - semiPrimesPreSum(p_val-1)
   }
 
-  def semiPrimesList(x: Int): List[Int] = {
+  def semiPrimesList(x: Int): Array[Int] = {
     val result = Array.fill[Int](x+1)(0)
     var index = 2
     while(index * index <= x){
@@ -51,18 +43,21 @@ object CountSemiprimes extends App{
       index += 1
     }
 
-    val semiPrimes = scala.collection.mutable.ArrayBuffer[Int]()
+    val semiPrimesPreSum = Array.fill[Int](x+1)(0)
+    var count = 0
     for(k <- (0 to x)){
       if(result(k) != 0){
         if(k % result(k) == 0){
           val p = k / result(k)
           if(result(p) == 0){
-            semiPrimes += k
+            count += 1
           }
         }
       }
+      semiPrimesPreSum(k) = count
     }
-    return semiPrimes.toList
+    
+    return semiPrimesPreSum
   }
 
 
